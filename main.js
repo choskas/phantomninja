a7869f3b298bdc4b661cf6fe896b532eb5ebc238
Choskas <choskasdelta@hotmail.com>
2019-08-28 13:44:44 -0500

    ya casi

--- main.js
+++ main.js
@@ -4,18 +4,21 @@ let gameBoard = document.querySelector('#principal')
 const titleAudio = document.querySelector('#titlemusic')
 const walkAudio = document.querySelector('#walk')
 const startAudio = document.querySelector('#startsound')
+const slashAudio = document.querySelector('#slash')
 const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
 let frames = 0
 let score = 0
-let contenemies=0
+let deadEnemies = 0
+let contenemies = 0
 let badCharacters = []
+let sunCoins = []
 //enemies
 
 
 window.onload = function () {
     gameBoard.style.display = 'none'
-  
+
 }
 
 
@@ -27,7 +30,7 @@ class Board {
         this.width = canvas.width
         this.height = canvas.height
         this.img = new Image()
-        this.img.src = './assets/background.jpg'
+        this.img.src = './assets/background2.png'
         this.img.onload = () => {
             this.draw()
         }
@@ -53,38 +56,52 @@ class Player {
         ctx.drawImage(this.img, this.x, this.y, this.width, this.height)
     }
     moveUp() {
-        this.y -= 20
+        this.y -= 30
     }
     moveDown() {
-        this.y += 20
+        this.y += 30
     }
     moveRight() {
-        this.x += 20
+        this.x += 30
     }
     moveLeft() {
-        this.x -= 20
+        this.x -= 30
     }
-    
-    
+
+    isTouching(enemy) {
+        return (
+            this.x < enemy.x + enemy.width &&
+            this.x + this.width > enemy.x &&
+            this.y < enemy.y + enemy.height &&
+            this.y + this.height > enemy.y
+        )
+    }
+    isTouching(sunCoins) {
+        return (
+            this.x < sunCoins.x + sunCoins.width &&
+            this.x + this.width > sunCoins.x &&
+            this.y < sunCoins.y + sunCoins.height &&
+            this.y + this.height > sunCoins.y
+        )
+    }
 }
 
 
 
-class enemigoss{
-    constructor (x,y,imagen)
-    {
-        this.x=x
-        this.y=y
-        this.width=100
-        this.height=150
-        this.img=new Image()
+class enemigoss {
+    constructor(x, y, imagen) {
+        this.x = x
+        this.y = y
+        this.width = 90
+        this.height = 100
+        this.img = new Image()
 
-        this.img.src=imagen
-        this.alive=true
+        this.img.src = imagen
+        this.alive = true
 
     }
-    draw(){
-        ctx.drawImage(this.img,this.x,this.y,this.width,this.height)
+    draw() {
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height)
     }
 
 }
@@ -101,7 +118,22 @@ class Warrior {
         this.img.src = './assets/warrior.png'
 
     }
-    draw(){
+    draw() {
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height)
+    }
+}
+
+class Sun {
+    constructor(x, y) {
+        this.x = x
+        this.y = y
+        this.width = 40
+        this.height = 50
+        this.img = new Image()
+        this.img.src = './assets/sun.png'
+        this.alive = true
+    }
+    draw() {
         ctx.drawImage(this.img, this.x, this.y, this.width, this.height)
     }
 }
@@ -114,28 +146,65 @@ const warrior = new Warrior(800, 200)
 
 
 
-function titleSound(){
+function titleSound() {
     titleAudio.play()
 }
 
-function walkSound (){
+function walkSound() {
     walkAudio.play()
 }
 
-function startBeep(){
+function startBeep() {
     startAudio.play()
-    
+
+}
+
+function slashSound() {
+    slashAudio.play()
 }
 
 
 
+function generateSun() {
+    if (frames % 200 === 0) {
+        let x = canvas.width
+        let y = canvas.height
+        let randomX = Math.floor((Math.random() * x) + 1)
+        let randomY = Math.floor((Math.random() * y) + 1)
+
+        sunCoins.push(new Sun(randomX, randomY))
+    }
+}
+
+function drawSun() {
+    sunCoins.forEach(suns => {
+        suns.alive = true
+        suns.draw()
+    })
+}
+
+function getSun() {
+    sunCoins.forEach(suns => {
+        if (player1.isTouching(suns)) return countSuns()
+
+
+
+    })
+
+
+}
+
+function countSuns() {
+    sunCoins.alive
+    score++
+}
 
 
 function generateEnemie() {
 
     enemy = new Array()
 
-    
+
     enemy[0] = './assets/Enemies/big_demon_idle_anim_f0.png'
     enemy[1] = './assets/Enemies/big_zombie_idle_anim_f0.png'
     enemy[2] = './assets/Enemies/masked_orc_idle_anim_f0.png'
@@ -144,13 +213,13 @@ function generateEnemie() {
     enemy[5] = './assets/Enemies/wizzart_m_hit_anim_f0.png'
     enemy[6] = './assets/Enemies/zombie_idle_anim_f1.png'
 
-   let selecciona= selectEnemy()
-  
-   
+    let selecciona = selectEnemy()
 
-   let x = canvas.width
-   let y = canvas.height
-   let i=new Image()
+
+
+    let x = canvas.width
+    let y = canvas.height
+    let i = new Image()
     let texto
     let valores = new Array()
 
@@ -158,40 +227,46 @@ function generateEnemie() {
     if (frames % 200 === 0) {
         let randomX = Math.floor((Math.random() * x) + 1)
         let randomY = Math.floor((Math.random() * y) + 1)
-     
-      texto =enemy[selecciona]
-    valores[0]=randomX
-    valores[1]=randomY
-    valores[2]=texto
-  
 
-  badCharacters.push(new enemigoss(valores[0],valores[1],valores[2]))
-  
- 
+        texto = enemy[selecciona]
+        valores[0] = randomX
+        valores[1] = randomY
+        valores[2] = texto
 
-//        const enemigos= new Enemies(randomX,randomY,texto)
-  //      enemigos.draw()
-        
-        
+
+        badCharacters.push(new enemigoss(valores[0], valores[1], valores[2]))
+
+
+
+        //        const enemigos= new Enemies(randomX,randomY,texto)
+        //      enemigos.draw()
+
+
     }
 }
 
-function selectEnemy(){
+function selectEnemy() {
     let aleatorio = Math.floor((Math.random() * 100) + 1)
     let selection = aleatorio % 7
-    return selection 
+    return selection
 }
 
 
 
 function drawScore() {
-    if (frames % 200 === 0) {
-        score += 2
-    }
+
     ctx.fillStyle = 'white'
     ctx.font = '28px Courier'
-    ctx.fillText(`Score: ${score}`, (canvas.width / 2) + 100, 50)
+    ctx.fillText(`Sun Coins: ${score}`, (canvas.width / 2) + 100, 50)
 }
+
+function drawScore2() {
+
+    ctx.fillStyle = 'white'
+    ctx.font = '28px Courier'
+    ctx.fillText(`Dead Enemies: ${deadEnemies}`, (canvas.width / 2) - 300, 50)
+}
+
 
 function start() {
     menu.style.display = 'none'
@@ -207,76 +282,96 @@ function update() {
     board.draw()
     player1.draw()
     warrior.draw()
-   generateEnemie() 
-   
-   drawenemies()
-   
+    generateEnemie()
+    drawenemies()
+    generateSun()
+    drawSun()
+    getSun()
     drawScore()
+    drawScore2()
+    killPlayer()
+
+}
+
+function drawenemies() {
+    badCharacters.forEach(character => {
+
+        if (character.alive == true)
+            character.draw()
+
+    })
 
 
 }
-function drawenemies() 
-{
-    badCharacters.forEach(character =>{
-        
-        if (character.alive==true)
-        character.draw()
-        
-       })
-    
 
+function gameOver() {
+    ctx.font = '30px Courier'
+    ctx.fillText(`Game Over killed Enemies: ${deadEnemies} Sun Coins: ${score}`, canvas.width / 2 - 400, 400)
+    ctx.font = '30px Courier'
+    ctx.fillText(`Press "P" to restart!`, canvas.width / 2, 600)
+    clearInterval(interval)
 }
 
 //funcion killplayer = misma funcion
+function killPlayer() {
+    badCharacters.forEach(character => {
+        if (player1.isTouching(character)) return gameOver()
+    })
+}
 
-function killenemie(xclick,yclick)
-{
-    let xmin=xclick-100
-    let xmax=xclick+100
-    let ymin=yclick-100
-    let ymax=yclick+100
 
-    badCharacters.forEach(character=>{
-        if ((character.x>=xmin && character.x<= xmax) && (character.y>=ymin && character.y<= ymax) )
-        {
-            character.alive=false
+
+function killenemie(xclick, yclick) {
+    let xmin = xclick - 100
+    let xmax = xclick + 100
+    let ymin = yclick - 100
+    let ymax = yclick + 100
+
+    badCharacters.forEach(character => {
+        if ((character.x >= xmin && character.x <= xmax) && (character.y >= ymin && character.y <= ymax)) {
+            character.alive = false
+            deadEnemies++
 
         }
     })
 
 }
-function MoveWarrior(x,y)
-{
 
-    warrior.x=x
-    warrior.y=y
+function MoveWarrior(x, y) {
+    slashSound()
+    warrior.x = x
+    warrior.y = y
 }
 //movimiento mouse 
 
-    
-    function getMousePos(canvas, evt) {
-        var rect = canvas.getBoundingClientRect()
-      return {
+
+function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect()
+    return {
         x: evt.clientX - rect.left,
         y: evt.clientY - rect.top,
 
-        
-      }
+
     }
+}
 
-    canvas.addEventListener('click', function(evt) {
-      var mousePos = getMousePos(canvas, evt);
-      var message = 'Mouse position: ' + mousePos.x + ',' + mousePos.y;
-      console.log(message);
-     MoveWarrior(mousePos.x,mousePos.y)
-     killenemie(mousePos.x, mousePos.y)
+canvas.addEventListener('click', function (evt) {
+    var mousePos = getMousePos(canvas, evt);
+    var message = 'Mouse position: ' + mousePos.x + ',' + mousePos.y;
+    console.log(message);
+    MoveWarrior(mousePos.x, mousePos.y)
+    killenemie(mousePos.x, mousePos.y)
 
-     
-     
-    }, false);
 
-  
 
+}, false);
+
+
+function restart() {
+    if (true) {
+        location.reload()
+    }
+}
 
 
 document.onkeydown = event => {
@@ -297,13 +392,17 @@ document.onkeydown = event => {
             player1.moveRight()
             walkSound()
             break
-            case 13:
-                titleAudio.pause()
-                start()
-                startBeep()
-                break
-            case 71:
-                titleSound()
+        case 13:
+            titleAudio.pause()
+            start()
+            startBeep()
+            break
+        case 71:
+            titleSound()
+            break
+        case 80:
+            restart()
+            break
     }
 
 }
